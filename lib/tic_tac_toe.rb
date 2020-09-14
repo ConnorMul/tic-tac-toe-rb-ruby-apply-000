c895ae57b276325b97c2619083e7f3a24b27733b
Connor Mulholland <mulhollandconnor@icloud.com>
2020-09-14 03:44:29 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -17,7 +17,7 @@ def move(board, index, current_player = "X")
 end
 
 def position_taken?(board, index)
-  board[index] = " " && board[index] == ""
+  board[index] = "X" || board[index] == "O"
 end
 
 def valid_move?(board, index)
